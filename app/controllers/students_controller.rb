52e94dc9f27aa04156f6a4d92dc849587be5aaad
Augusto Granitto <augustogranitto@gmail.com>
2018-09-15 01:55:41 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,6 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
@@ -9,11 +9,17 @@ class StudentsController < ApplicationController
     set_student
   end
 
+  def activate 
+    set_student
+    @stundent.activate
+    redirect_to student_path(@student)
+  end 
+
   private
 
     def set_student
       @student = Student.find(params[:id])
     end
 
-    
+
 end
